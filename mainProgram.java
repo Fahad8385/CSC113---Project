f3eacc58dcbbc536517b26694de4e38efc0bad50
Fahad <fahadalgroany@gmail.com>
2024-10-09 15:47:02 +0300

    Version 2.1

--- mainProgram.java
+++ mainProgram.java
@@ -6,20 +6,20 @@ public class mainProgram {
 
 		Flight f2 = new Flight("Riyadh", "ALUla", 1040.6);
 		System.out.println(f2.getflightNumber());
-		// Scanner input = new Scanner(System.in);
-		// boolean state = true;
+		Scanner input = new Scanner(System.in);
+		boolean state = true;
 		
-		// while(state) {
-		// 	System.out.println("======Welcome to Loyalty Program======");
-		// 	System.out.println("1. Register a new member");
-		// 	System.out.println("2. Login");
-		// 	System.out.println("Q. Quit Program");
+		while(state) {
+			System.out.println("======Welcome to Loyalty Program======");
+			System.out.println("1. Register a new member");
+			System.out.println("2. Login");
+			System.out.println("Q. Quit Program");
 			
-		// 	String choice = input.next();
-		// 	switch (choice) {
-		// 	case "1":
+			String choice = input.next();
+			switch (choice) {
+			case "1":
 				
-		// 	}
-		// }
+			}
+		}
 	}
 }
