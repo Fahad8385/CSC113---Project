015a80b9acb3ae3262d6b4f791fea794d2005a81
Fahad <fahadalgroany@gmail.com>
2024-10-10 15:06:05 +0300

    Version 3.0

---
+++ SilverMember.java
@@ -0,0 +1,34 @@
+
+public class SilverMember extends Member{
+
+	// Parameterized Constructor
+	public SilverMember(String name, String username, String password) {
+		super(name, username, password);
+	}
+	
+	// No-Arguments Constructor
+	public SilverMember() {};
+	
+	// Copy Constructor
+	public SilverMember (Member member) {
+		this.name = member.name;
+		this.userName = member.userName;
+		this.password = member.password;
+		this.points = member.points;
+	}
+//	@Override
+//	public Member createMemeber(String name, String username, String password) {
+//		
+//	}
+
+	@Override
+	public void setPoints(int distance) {
+		
+	}
+
+	@Override
+	public void substractPoints(double distance) {
+		
+	}
+	
+}
